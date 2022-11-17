a3e6e962163639958252b8863628fb945a888d5d
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2022-11-17 15:41:18 +0100

    Lint

--- biketrauma/io/Load_db.py
+++ biketrauma/io/Load_db.py
@@ -5,11 +5,16 @@ from biketrauma.io import url_db, path_target
 
 
 class Load_db:
-  def __init__(self, url=url_db, target_name=path_target):
-    path, fname = os.path.split(path_target)
-    pooch.retrieve(url, path=path, fname=fname, known_hash=None)
-  
-  @staticmethod
-  def save_as_df():
-    df_bikes = pd.read_csv(path_target, na_values="", low_memory=False, converters={'data': str, 'heure': str})
-    return df_bikes
+    def __init__(self, url=url_db, target_name=path_target):
+        path, fname = os.path.split(path_target)
+        pooch.retrieve(url, path=path, fname=fname, known_hash=None)
+
+    @staticmethod
+    def save_as_df():
+        df_bikes = pd.read_csv(
+            path_target,
+            na_values="",
+            low_memory=False,
+            converters={"data": str, "heure": str},
+        )
+        return df_bikes
